2805669d794beb70b908fd473e5ed2de3ef90631
Felipe Alvarenga <felipe.alvarenga01@yahoo.com.br>
2023-01-15 11:04:11 -0300

    insercao das pastas faltantes

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, BrowserRouter, Switch} from 'react-router-dom';
+
+import Logon from "./pages/Logon";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import NewIncident from "./pages/NewIncident";
+
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+    <Switch>
+      <Route component={Logon} path="/" exact />
+      <Route component={Register} path="/register"/>
+      <Route component={Profile} path="/profile"/>
+      <Route component={NewIncident} path="/incidents/new"/>
+    </Switch>
+    </BrowserRouter>
+  );
+}
